a42e667afffd783c10459ee260e66774937b7026
xDp64xGaming <brow.paul64@gmail.com>
2026-08-26 23:44:07 -0400

    Update Torn OC CRP Fit.user.js

--- scripts/tampermonkey/Torn OC CRP Fit.user.js
+++ scripts/tampermonkey/Torn OC CRP Fit.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Torn OC CRP Fit
 // @namespace    http://tampermonkey.net/
+// @author       JeffBezas
 // @version      1.5.1
 // @description  Highlights the organised crime slots that best fit your CPR, using the faction CRP/weight table.
 // @match        https://www.torn.com/factions.php?step=your&type=1*
